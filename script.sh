f4cef2b560da5ea5dc6b34dcf58314f0c8d15253
mofa <ngekemofa@gmail.com>
2023-09-16 14:39:04 +0100

    contributing to a project

--- script.sh
+++ script.sh
@@ -3,4 +3,5 @@ read name
 echo -n "Please enter your team"
 read team
 echo "OHH Nice, welcome to Etech Devops master class $name and you are welcome to $team"
-echo "End of script"
+echo "End of script
+echo "This is git on visual studio code""
