9119b3ffb70c4684b8cde608f392e7134045a4b2
mofa <ngekemofa@gmail.com>
2023-09-16 15:08:03 +0100

    comments added to script.sh file

--- script.sh
+++ script.sh
@@ -5,3 +5,4 @@ read team
 echo "OHH Nice, welcome to Etech Devops master class $name and you are welcome to $team"
 echo "End of script
 echo "This is git on visual studio code""
+#Kindly review this Engr 
